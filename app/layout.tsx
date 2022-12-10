fefd4be08eb9175ec95d8cec3979b7c09236c280
alexmagwe <alexmagwe@gmail.com>
2022-12-10 12:24:16 +0300

    update:added tailwindcss and prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
       <body>{children}</body>
+      <footer className='position absolute bottom-0'>
+        <a
+          href='https://keplalabs.co.ke'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <span className=''>@keplalabs</span>
+        </a>
+      </footer>
     </html>
   )
 }
